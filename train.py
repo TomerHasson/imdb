d7bd8ef37a64444690bce018a305ee6d8b4b4d9f
TomerHasson <52401321+TomerHasson@users.noreply.github.com>
2019-08-08 10:42:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,2 +1,11 @@
-print("bug check")
-exit(1)
+import time
+from cnvrg import Experiment
+i = 0
+while True:
+    filename = "test-{file_idx}.log".format(file_idx=i)
+    f = open(filename, "a")
+    f.write("hello")
+    f.close()
+    Experiment.sync(message="my commit: %d" % i)
+    time.sleep(60)
+    i += 1
