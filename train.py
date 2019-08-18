e3e86d52c77e5c5007647a9e811441065078829e
TomerHasson <52401321+TomerHasson@users.noreply.github.com>
2019-08-18 10:09:35 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,1 @@
-import time
-import os
-from cnvrg import Experiment
-os.mkdir('output')
-i = 10
-while True:
-    filename = "output/test-{file_idx}.log".format(file_idx=i)
-    f = open(filename, "a")
-    f.write("hello")
-    f.close()
-    Experiment.sync(message="my commit: %d" % i)
-    time.sleep(60)
-    i += 1
+exit(1)
