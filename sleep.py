c6adf6ff8384c49524c4eaa28a7c10fecf429cc3
TomerHasson <52401321+TomerHasson@users.noreply.github.com>
2020-02-18 10:04:31 +0200

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,5 +1,6 @@
 import time
-f = open("first_exp.txt", "a")
+print("creating file)
+f = open("output/first_exp.txt", "a")
 f.write("Now the file has more content!")
 f.close()
-time.sleep(60000000)
+time.sleep(10)
